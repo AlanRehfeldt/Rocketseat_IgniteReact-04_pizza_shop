20b7cf098f286ea96f421102c48471e825d847be
Alan Rehfeldt <alanrehfeldt@gmail.com>
2024-03-09 22:22:04 -0300

    pagina cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -30,7 +31,7 @@ export function SignIn() {
         },
       })
     } catch (e) {
-      toast.error('Ocorreu um erro ao enviar o email.')
+      toast.error('Credencias inválidas.')
     }
   }
 
@@ -38,6 +39,9 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button asChild className="absolute right-8 top-8" variant="ghost">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
