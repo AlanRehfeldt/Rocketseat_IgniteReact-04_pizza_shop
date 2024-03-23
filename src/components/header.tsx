31a746482149ed490f5b9c071697d88d72e5377d
Alan Rehfeldt <alanrehfeldt@gmail.com>
2024-03-22 21:50:04 -0300

    loading dos pedidos

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,7 +17,7 @@ export function Header() {
             <Home className="h-4 w-4" />
             Inicio
           </NavLink>
-          <NavLink to="/orders">
+          <NavLink to="/orders?page=1">
             <UtensilsCrossed className="h-4 w-4" />
             Pedidos
           </NavLink>
