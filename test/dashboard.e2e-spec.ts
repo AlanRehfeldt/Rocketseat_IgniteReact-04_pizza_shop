5962fd2875c7b0f0be38f7279f25758ff0f03b0e
Alan Rehfeldt <alanrehfeldt@gmail.com>
2024-03-25 20:22:52 -0300

    teste e2e: metricas do dashboard

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test'
+
+test('display day orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.waitForTimeout(1000)
+
+  expect(page.getByText('20', { exact: true })).toBeVisible()
+  expect(page.getByText('-5%').first()).toBeVisible()
+})
+
+test('display month orders amountm etric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('200', { exact: true })).toBeVisible()
+  expect(page.getByText('+7% em relação ao mês passado')).toBeVisible()
+})
+
+test('display month canceled orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('5', { exact: true })).toBeVisible()
+  expect(page.getByText('-5%').nth(1)).toBeVisible()
+})
+
+test('display month revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('200', { exact: true })).toBeVisible()
+  expect(page.getByText('+10% em relação ao mês passado')).toBeVisible()
+})
